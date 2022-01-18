e72c53d89d7cb336d73886c47bdb1e3b3f56856b
jyb8509 <bestdung8509@gmail.com>
2022-01-18 13:20:28 +0900

    GNB 진행률 1/18일 30%

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,21 @@
 import React from "react";
 import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyles = createGlobalStyle`
+    ${reset}
+    * {
+    box-sizing: border-box;
+    font-size: 14px;
+    font-family: "sans-serif";
+  }
+`;
 
 const Nav = () => {
   return (
     <>
+      <GlobalStyles />
       <Header>
         <Inner>
           <Mainbar>
@@ -27,9 +39,31 @@ const Nav = () => {
 
             <Iconbar>
               <Listbar>
-                <ListIcon></ListIcon>
-                <ListIcon></ListIcon>
-                <ListIcon></ListIcon>
+                <ListIcon>
+                  <ManyImage
+                    src="https://media.vlpt.us/images/yeonbee/post/cdc45b5a-757f-420a-8339-89c674c7d5cf/%EA%B2%80%EC%83%89.png"
+                    alt="검색버튼"
+                  />
+                </ListIcon>
+                <ListIcon>
+                  <ManyImage
+                    src="https://media.vlpt.us/images/yeonbee/post/ae98fc88-c182-49a6-87a8-39a56dd32b73/%EC%A2%85.png"
+                    alt="알림버튼"
+                  />
+                </ListIcon>
+                <ListIcon>
+                  <ManyImage
+                    src="https://media.vlpt.us/images/yeonbee/post/6daea6b8-bdf2-44b9-8e43-7fbac3bdca57/%EC%95%84%EB%B0%94%ED%83%80.png"
+                    alt="아바타버튼"
+                  />
+                </ListIcon>
+
+                <ListIcon>
+                  <ManyImage
+                    src="https://media.vlpt.us/images/yeonbee/post/d8c65170-090a-4a98-84d7-52d456bfbab5/%EA%B8%B0%EC%97%85%EC%84%9C%EB%B9%84%EC%8A%A4.png"
+                    alt="아바타버튼"
+                  />
+                </ListIcon>
               </Listbar>
             </Iconbar>
           </Mainbar>
@@ -87,6 +121,8 @@ const Listbar = styled.ul`
 const ListName = styled.li`
   padding: 15px;
   line-height: 20px;
+  font-size: 14px;
+  font-family: sans-serif;
   font-weight: 600;
   display: inline-block;
   vertical-align: middle;
@@ -94,4 +130,10 @@ const ListName = styled.li`
 
 const Iconbar = styled.div``;
 
-const ListIcon = styled.li``;
+const ListIcon = styled.li`
+  vertical-align: middle;
+`;
+
+const ManyImage = styled.img`
+  width: 60%;
+`;
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,9 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyles = createGlobalStyle`
+    ${reset}
+    * {
+    box-sizing: border-box;
+    font-size: 14px;
+    font-family: "sans-serif";
+  }
+`;
+
 const Main = () => {
   return (
     <div>
+      <GlobalStyles />
       <MainSlide>메인 페이지입니다_슬라이드</MainSlide>
     </div>
   );
