31283dc70a97983f22e48ee0f521b6bab843913d
jyb8509 <bestdung8509@gmail.com>
2022-01-19 18:22:15 +0900

    1/19일 GNB 반응형 진행률 30%

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,7 +5,11 @@ import reset from "styled-reset";
 import Button from "./Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell } from "@fortawesome/free-regular-svg-icons";
-import { faSearch, faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSearch,
+  faUserCircle,
+  faEllipsisH,
+} from "@fortawesome/free-solid-svg-icons";
 
 const GlobalStyles = createGlobalStyle`
     ${reset}
@@ -32,13 +36,14 @@ const Nav = () => {
             </Logobar>
 
             <Listbar>
+              <ListNameMobile>홈</ListNameMobile>
               <ListName>채용</ListName>
               <ListName>이벤트</ListName>
-              <ListName>직군별 연봉</ListName>
-              <ListName>이력서</ListName>
-              <ListName>커뮤니티</ListName>
-              <ListName>프리랜서</ListName>
-              <ListName>AI 합격예측</ListName>
+              <ListNameWeb>직군별 연봉</ListNameWeb>
+              <ListNameWeb>이력서</ListNameWeb>
+              <ListNameWeb>커뮤니티</ListNameWeb>
+              <ListNameWeb>프리랜서</ListNameWeb>
+              <ListNameWeb>AI 합격예측</ListNameWeb>
             </Listbar>
 
             <Listbar>
@@ -61,27 +66,35 @@ const Nav = () => {
                   />
                 </Button>
               </ListIcon>
-              <ListIcon>
+              <ListIconMobile>
+                <Button>
+                  <FontAwesomeIcon
+                    icon={faEllipsisH}
+                    style={{ fontSize: "18px", color: "#494848" }}
+                  />
+                </Button>
+              </ListIconMobile>
+              <ListIconWeb>
                 <Button>
                   <FontAwesomeIcon
                     icon={faUserCircle}
                     style={{ fontSize: "28px", color: "#0F6C44" }}
                   />
                 </Button>
-              </ListIcon>
-              <ListIcon>
+              </ListIconWeb>
+              <ListIconWeb>
                 <Button size="small">
                   <InnerButtonText>ㅣ</InnerButtonText>
                 </Button>
-              </ListIcon>
-              <ListIcon>
+              </ListIconWeb>
+              <ListIconWeb>
                 <Button size="large" radius="circle">
                   <ManyImageCservice
                     src="https://media.vlpt.us/images/yeonbee/post/d8c65170-090a-4a98-84d7-52d456bfbab5/%EA%B8%B0%EC%97%85%EC%84%9C%EB%B9%84%EC%8A%A4.png"
                     alt="기업서비스"
                   />
                 </Button>
-              </ListIcon>
+              </ListIconWeb>
             </Listbar>
           </Mainbar>
         </Inner>
@@ -139,6 +152,18 @@ const Listbar = styled.ul`
   display: flex;
 `;
 
+const ListNameMobile = styled.li`
+  padding: 15px;
+  line-height: 20px;
+  font-size: 14px;
+  font-family: sans-serif;
+  font-weight: 600;
+  display: inline-block;
+  @media screen and (min-width: 767px) {
+    display: none;
+  }
+`;
+
 const ListName = styled.li`
   padding: 15px;
   line-height: 20px;
@@ -148,8 +173,31 @@ const ListName = styled.li`
   display: inline-block;
 `;
 
+const ListNameWeb = styled.li`
+  padding: 15px;
+  line-height: 20px;
+  font-size: 14px;
+  font-family: sans-serif;
+  font-weight: 600;
+  display: inline-block;
+  @media screen and (max-width: 767px) {
+    display: none;
+  }
+`;
+
 // ListIcon 영역
 const ListIcon = styled.li``;
+
+const ListIconMobile = styled.li`
+  @media screen and (min-width: 767px) {
+    display: none;
+  }
+`;
+const ListIconWeb = styled.li`
+  @media screen and (max-width: 767px) {
+    display: none;
+  }
+`;
 
 const InnerButtonText = styled.div`
   color: lightgray;
